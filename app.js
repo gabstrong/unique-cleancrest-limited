d05c8e5dd36be2cc2d1e59b7c6ee6d11b4ab13fa
[gabstrong] <freedomgabriel77@gmail.com>
2025-10-22 01:14:14 +0100

    uploading file at first (continue...)

---
+++ app.js
@@ -0,0 +1,22 @@
+//  TOGGLE MENU......
+
+ const menuList = document.querySelector('#menulist');
+ const dropList = document.getElementById('hamburger');
+ 
+    
+
+  function openMenu(){
+   menuList.classList.toggle('close_menu');
+
+
+     document.addEventListener('click', e => {
+    if (menuList.contains(e.target) ) {
+
+      menuList.classList.remove('close_menu');
+
+    }
+  });
+  };
+  
+  
+
